4f9a0c29ca14b2ba4a3612d2def27b2b35347588
Muhammad Suleman <36508618+sallu08@users.noreply.github.com>
2024-06-22 13:34:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,48 @@
 import ollama
 import streamlit as st
 
-st.title("LLAMA3 ChatBot")
+st.title("Moron ChatBot")
 
-def generate_response(user_message):
-    response = ollama.chat(
+def generate_response(user_message: str, chat_history: list=[]):
+    system_msg=("You are a Chatbot assistant")       #give role to Chatbot 
+    my_message = [{"role": "system", "content": system_msg}]
+    
+    for chat in chat_history:                        #Append history in message 
+        my_message.append({"role": chat["name"], "content": chat["msg"]})
+                                                     #Append the latest question in message
+    my_message.append({"role": "user", "content": user_message})
+    
+    response = ollama.chat(                          #define model&input for response 
         model="llama3",
-        messages=[{"role": "user", "content": user_message}]
-    )
+        messages=my_message
+        )
+
     return response["message"]["content"]
 
+def main():
 
-user_msg = st.chat_input("Enter your message here", key="user_input")
-if user_msg:
-    with st.chat_message("user"):
-        st.write(user_msg)
+    if "chat_log" not in st.session_state:
+        st.session_state.chat_log = []
 
-response=generate_response(user_msg)
+    user_message = st.chat_input("Enter your message here", key="user_input")
+    if user_message:
+        for chat in st.session_state.chat_log:          #first display chat history
+            with st.chat_message(chat["name"]):
+                st.write(chat["msg"])
+    
+        with st.chat_message("user"):                  #display latest user message
+            st.write(user_message)
 
-if response:
-    with st.chat_message("assistant"):
-        assistant_response_area = st.empty()
-        assistant_response_area.write(response)
+    response=generate_response(user_message, chat_history=st.session_state.chat_log)
+
+    if response:
+        with st.chat_message("assistant"):
+            assistant_response_area = st.empty()
+            assistant_response_area.write(response)   # at bottom show LLM response
+        
+        #add latest message and response to chat history 
+        st.session_state.chat_log.append({"name": "user", "msg": user_message})
+        st.session_state.chat_log.append({"name": "assistant", "msg": response})
+
+if __name__ == "__main__":
+    main()
